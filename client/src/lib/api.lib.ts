13a7c755bd981d2c6fe2a5548be5ffa9e6588725
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-07-08 15:44:53 -0300

    Chore: initial commit

--- client/src/lib/api.lib.ts
+++ client/src/lib/api.lib.ts
@@ -10,8 +10,6 @@ const api = axios.create({
 let memmoryAccessToken: Maybe<string> = undefined;
 
 api.interceptors.request.use(async (req) => {
-  console.log({ memmoryAccessToken });
-
   if (memmoryAccessToken) {
     req.headers.Authorization = `Bearer ${memmoryAccessToken}`;
   } else {

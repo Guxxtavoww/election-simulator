943477d6041efb9eafe42943a9ba88a0692038b4
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-07-15 17:41:14 -0300

    Chore: refactoring

--- server/src/modules/politician/dtos/list-politicians.dto.ts
+++ server/src/modules/politician/dtos/list-politicians.dto.ts
@@ -2,34 +2,25 @@ import { z } from 'nestjs-zod/z';
 import { createZodDto } from 'nestjs-zod';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
-import {
-  optionalOrderParamSchema,
-  optionalStringSchemaToLowerCase,
-} from 'src/shared/schemas.shared';
 import { OrderByEnum } from 'src/shared/enums.shared';
+import { optionalOrderParamSchema, optionalStringSchemaToLowerCase } from 'src/shared/schemas.shared';
 
-import {
-  optionalIdeologySchema,
-  PoliticianPoliticalIdeology,
-} from '../enums/politician-political-ideology.enum';
-import {
-  PoliticianType,
-  optionalPoliticianTypeSchema,
-} from '../enums/politician-type.enum';
+import { optionalPoliticianTypeSchema, PoliticianType } from '../enums/politician-type.enum';
+import { optionalIdeologySchema, PoliticianPoliticalIdeology } from '../enums/politician-political-ideology.enum';
 
-export const listPoliticiansSchema = z.object({
+export const fields = {
   politician_name: optionalStringSchemaToLowerCase,
   political_ideology: optionalIdeologySchema,
   order_by_date_of_birth: optionalOrderParamSchema,
   order_by_most_votes: optionalOrderParamSchema,
   politician_type: optionalPoliticianTypeSchema,
-});
+} as const;
+
+export const listPoliticiansSchema = z.object(fields);
 
 export type ListPoliticiansType = z.infer<typeof listPoliticiansSchema>;
 
-export class ListPoliticiansDTO extends createZodDto(
-  listPoliticiansSchema,
-) {
+export class ListPoliticiansDTO extends createZodDto(listPoliticiansSchema) {
   @ApiPropertyOptional({
     description: 'Name of the politician',
     type: String,
--- server/src/modules/politician/dtos/paginate-politicians.dto.ts
+++ server/src/modules/politician/dtos/paginate-politicians.dto.ts
@@ -1,67 +1,12 @@
 import { z } from 'nestjs-zod/z';
-import { createZodDto } from 'nestjs-zod';
-import { ApiPropertyOptional } from '@nestjs/swagger';
 
-import {
-  optionalOrderParamSchema,
-  optionalStringSchemaToLowerCase,
-} from 'src/shared/schemas.shared';
-import { OrderByEnum } from 'src/shared/enums.shared';
 import { createPaginationSchemaWithoutOrderBy } from 'src/utils/create-pagination-schema.utils';
 
-import {
-  optionalIdeologySchema,
-  PoliticianPoliticalIdeology,
-} from '../enums/politician-political-ideology.enum';
-import {
-  PoliticianType,
-  optionalPoliticianTypeSchema,
-} from '../enums/politician-type.enum';
+import { fields, ListPoliticiansDTO } from './list-politicians.dto';
 
-export const paginatePoliticiansSchema = createPaginationSchemaWithoutOrderBy({
-  politician_name: optionalStringSchemaToLowerCase,
-  political_ideology: optionalIdeologySchema,
-  order_by_date_of_birth: optionalOrderParamSchema,
-  order_by_most_votes: optionalOrderParamSchema,
-  politician_type: optionalPoliticianTypeSchema,
-});
+export const paginatePoliticiansSchema =
+  createPaginationSchemaWithoutOrderBy(fields);
 
 export type PaginatePoliticiansType = z.infer<typeof paginatePoliticiansSchema>;
 
-export class PaginatePoliticiansDTO extends createZodDto(
-  paginatePoliticiansSchema,
-) {
-  @ApiPropertyOptional({
-    description: 'Name of the politician',
-    type: String,
-    example: 'John Doe',
-  })
-  politician_name?: string;
-
-  @ApiPropertyOptional({
-    type: 'enum',
-    enum: PoliticianPoliticalIdeology,
-    example: PoliticianPoliticalIdeology.RIGHT,
-  })
-  political_ideology?: PoliticianPoliticalIdeology;
-
-  @ApiPropertyOptional({
-    type: 'enum',
-    enum: PoliticianType,
-  })
-  politician_type?: PoliticianType;
-
-  @ApiPropertyOptional({
-    type: 'enum',
-    enum: OrderByEnum,
-    example: OrderByEnum.DESC,
-  })
-  order_by_date_of_birth?: 'ASC' | 'DESC';
-
-  @ApiPropertyOptional({
-    type: 'enum',
-    enum: OrderByEnum,
-    example: OrderByEnum.DESC,
-  })
-  order_by_most_votes?: 'ASC' | 'DESC';
-}
+export class PaginatePoliticiansDTO extends ListPoliticiansDTO {}
